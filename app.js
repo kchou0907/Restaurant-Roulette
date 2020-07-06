2bab403e7754462fc263e88a86c869da096dd33e
kchou97 <kchou97@uw.edu>
2020-07-06 11:41:41 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,82 @@
+//jshint esversion: 6
+
+//google maps query example https://www.google.com/maps/dir/?api=1&origin=5557+San+Antonio+Street%2C+Pleasanton%2C+CA&destination=416+Barber+Ln%2C+Milpitas%2C+CA+95035&travelmode=walking&dir_action=navigate
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const dotenv = require('dotenv');
+const fetch = require('node-fetch');
+const bodyparser = require('body-parser');
+
+dotenv.config();
+
+const API_KEY = process.env.YELP_API_KEY;
+const GEOCODING_KEY = process.env.LOCATION_API_KEY;
+
+app.use(express.static("public"));
+
+// some middleware options for bodyparser
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({ extended: false }))
+
+app.listen(3000, function(req, res){
+  console.log("Server is running on port 3000");
+  console.log(API_KEY);
+  console.log(GEOCODING_KEY);
+});
+
+app.get('/categories', function(req, res){
+  getCategories()
+  .then(result => res.send(result))
+  .catch(error => console.log('error', error));
+});
+
+app.post('/autocomplete', function(req, res){
+  let typed = req.body.address;
+  console.log(req.body);
+});
+
+app.post("/", function(req,res){
+  console.log(req.body);
+  let data = req.body;
+
+  let location = req.body.address.split(",");
+  let cuisine = req.body.cuisine || "";
+  let price = req.body.price;
+
+  let distance = parseInt(data.transport);
+  let transportList = { 1: 'walking', 3: 'biking', 5: 'transit', 10: 'driving' };
+  let transport = transportList[distance];
+
+  console.log(location);
+});
+
+function getCategories() {
+  let requestOptions = {
+    method: 'GET',
+    headers:{
+      'Authorization' : 'Bearer ' + API_KEY
+    },
+    redirect: 'follow'
+  };
+
+  return fetch("https://api.yelp.com/v3/categories", requestOptions)
+    .then(checkStatus)
+    .then(result => result.json());
+}
+
+/**
+   * Helper function to return the response's result text if successful, otherwise
+   * returns the rejected Promise result with an error status and corresponding text
+   * @param {object} response - response to check for success/error
+   * @return {object} - valid response if response was successful, otherwise rejected
+   * Promise result
+   */
+  function checkStatus(response) {
+    if (response.ok) {
+      return response;
+    }
+    throw Error('Error in request: ' + response.statusText);
+
+  }
+
---
+++ public/index.js
@@ -0,0 +1,295 @@
+/**
+ * Name: _your name here_
+ * Date: _add date here_
+ * Section: CSE 154 _your section here_
+ *
+ * -- your description of what this file does here --
+ * Do not keep comments from this template in any work you submit (functions included under "Helper
+ * functions" are an exception, you may keep the function names/comments of id/qs/qsa/gen)
+ */
+
+//remember to use Bearer API_KEY
+"use strict";
+
+(function () {
+
+  /**
+   * Add a function that will be called when the window is loaded.
+   */
+  window.addEventListener("load", init);
+
+  /**
+   * CHANGE: Describe what your init function does here.
+   */
+  function init() {
+    getCategories();
+    id('location').addEventListener('click', getLocation);
+    name("address")[0].addEventListener("input", getAutocomplete);
+  }
+
+  /**
+   * Obtains all the cuisines from yelp
+   */
+  function getCategories() {
+    fetch('/categories')
+      .then(checkStatus)
+      .then(res => res.json())
+      .then(function (res) {
+        let restaurantOnly = res.categories.filter(function (entry) {
+          return entry.parent_aliases[0] === 'restaurants';
+        });
+        let titles = processRestaurants(restaurantOnly);
+        appendOption(titles);
+      })
+      .catch(error => console.log('error', error));
+  }
+
+  /**
+   * Goes through the JSON object and extracts all of the cuisine types
+   * @param {JSON} restaurants - JSON object returned from Yelp API call
+   * @returns {Array} - array of cuisine names
+   */
+  function processRestaurants(restaurants) {
+    let result = [];
+    for (let i in restaurants) {
+      result.push(restaurants[i].title);
+    }
+    return result;
+  }
+
+  /**
+   * Appends all the diffierent cuisines as options in the dropdown
+   * @param {Array} types - array of all the cuisine names
+   */
+  function appendOption(types) {
+    let cuisine = document.getElementById('cuisine');
+
+    //put different categories into dropdown
+    types.forEach(function (item, index) {
+      var option = gen('option');
+      option.innerHTML = item;
+      option.setAttribute('value', item);
+      cuisine.appendChild(option);
+    });
+  }
+
+
+  //location data
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+
+  function showPosition(position) {
+    let latlon = position.coords.latitude + ", " + position.coords.longitude;
+    name("address")[0].value = latlon;
+  }
+
+  function showError(error) {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        alert("User denied the request for Geolocation.");
+        break;
+      case error.POSITION_UNAVAILABLE:
+        alert(x.innerHTML = "Location information is unavailable.");
+        break;
+      case error.TIMEOUT:
+        alert(x.innerHTML = "The request to get user location timed out.");
+        break;
+      case error.UNKNOWN_ERROR:
+        alert(x.innerHTML = "An unknown error occurred.");
+        break;
+    }
+  }
+
+  //autocomplete address box
+  function getAutocomplete() {
+    let params = new FormData();
+    params.append("address", id('myInput').value);
+
+    // Display the key/value pairs
+    for (var pair of params.entries()) {
+      console.log(pair[0] + ', ' + pair[1]);
+    }
+    fetch('/autocomplete', {
+        method: 'POST',
+        body: params
+      })
+      .then(checkStatus)
+      .then(res => res.json())
+      .catch(error => console.log('error', error));
+  }
+
+  function autocomplete(inp, arr) {
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+    var currentFocus;
+    /*execute a function when someone writes in the text field:*/
+    inp.addEventListener("input", function (e) {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) {
+        return false;
+      }
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
+      for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+          b.addEventListener("click", function (e) {
+            /*insert the value for the autocomplete text field:*/
+            inp.value = this.getElementsByTagName("input")[0].value;
+            /*close the list of autocompleted values,
+            (or any other open lists of autocompleted values:*/
+            closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+    });
+    /*execute a function presses a key on the keyboard:*/
+    inp.addEventListener("keydown", function (e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+    });
+
+    function addActive(x) {
+      /*a function to classify an item as "active":*/
+      if (!x) return false;
+      /*start by removing the "active" class on all items:*/
+      removeActive(x);
+      if (currentFocus >= x.length) currentFocus = 0;
+      if (currentFocus < 0) currentFocus = (x.length - 1);
+      /*add class "autocomplete-active":*/
+      x[currentFocus].classList.add("autocomplete-active");
+    }
+
+    function removeActive(x) {
+      /*a function to remove the "active" class from all autocomplete items:*/
+      for (var i = 0; i < x.length; i++) {
+        x[i].classList.remove("autocomplete-active");
+      }
+    }
+
+    function closeAllLists(elmnt) {
+      /*close all autocomplete lists in the document,
+      except the one passed as an argument:*/
+      var x = document.getElementsByClassName("autocomplete-items");
+      for (var i = 0; i < x.length; i++) {
+        if (elmnt != x[i] && elmnt != inp) {
+          x[i].parentNode.removeChild(x[i]);
+        }
+      }
+    }
+    /*execute a function when someone clicks in the document:*/
+    document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+    });
+  }
+
+  /** ------------------------------ Helper Functions  ------------------------------ */
+  /**
+   * Note: You may use these in your code, but remember that your code should not have
+   * unused functions. Remove this comment in your own code.
+   */
+
+  /**
+   * Helper function to return the response's result text if successful, otherwise
+   * returns the rejected Promise result with an error status and corresponding text
+   * @param {object} response - response to check for success/error
+   * @return {object} - valid response if response was successful, otherwise rejected
+   * Promise result
+   */
+  function checkStatus(response) {
+    if (response.ok) {
+      return response;
+    }
+    throw Error('Error in request: ' + response.statusText);
+
+  }
+
+
+  /**
+   * Returns all the elements that has the name attribute with the specificied value
+   * @param {String} name the name
+   * @returns {NodeList} returns a list of all of the elements that share the specified name
+   */
+  function name(name) {
+    return document.getElementsByName(name);
+  }
+
+  /**
+   * Returns the element that has the ID attribute with the specified value.
+   * @param {string} idName - element ID
+   * @returns {object} DOM object associated with id.
+   */
+  function id(idName) {
+    return document.getElementById(idName);
+  }
+
+  /**
+   * Returns the first element that matches the given CSS selector.
+   * @param {string} selector - CSS query selector.
+   * @returns {object} The first DOM object matching the query.
+   */
+  function qs(selector) {
+    return document.querySelector(selector);
+  }
+
+  /**
+   * Returns the array of elements that match the given CSS selector.
+   * @param {string} selector - CSS query selector
+   * @returns {object[]} array of DOM objects matching the query.
+   */
+  function qsa(selector) {
+    return document.querySelectorAll(selector);
+  }
+
+  /**
+   * Returns a new element with the given tag name.
+   * @param {string} tagName - HTML tag name for new DOM element.
+   * @returns {object} New DOM object for given HTML tag.
+   */
+  function gen(tagName) {
+    return document.createElement(tagName);
+  }
+
+})();
