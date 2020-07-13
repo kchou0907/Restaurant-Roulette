1f74936e843d0a9657b2d0b53b6083ee1f6faf77
kchou97 <kchou97@uw.edu>
2020-07-13 01:44:10 -0700

    added navigation and autocomplete

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyparser.urlencoded({ extended: false }))
 app.listen(3000, function(req, res){
   console.log("Server is running on port 3000");
   console.log(API_KEY);
-  console.log(GEOCODING_KEY);
+
 });
 
 app.get('/categories', function(req, res){
@@ -31,30 +31,26 @@ app.get('/categories', function(req, res){
   .catch(error => console.log('error', error));
 });
 
-app.post('/autocomplete', function(req, res){
-  let typed = req.body.address;
-  console.log(typed);
-  getAutocomplete(typed)
-  .then(function(result){
-    console.log(result);
-    res.send(result);
-  })
-  .catch(error => console.log('error', error));
-});
-
 app.post("/", function(req,res){
   console.log(req.body);
   let data = req.body;
 
-  let from = req.body.address.split(",");
+  let from = req.body.address;
   let cuisine = req.body.cuisine || "";
   let price = req.body.price;
 
-  let distance = parseInt(data.transport);
-  let transportList = { 1: 'walking', 3: 'biking', 5: 'transit', 10: 'driving' };
-  let transport = transportList[distance];
+  let maxMiles = parseInt(data.transport);
+  let distance = Math.ceil(maxMiles * 1609.34);
+  let transportList = { 1: 'walking', 3: 'bicycling', 5: 'transit', 10: 'driving' };
+  let transport = transportList[maxMiles];
 
-  res.redirect('https://www.google.com/maps/dir/?api=1&origin=5557+San+Antonio+Street%2C+Pleasanton%2C+CA&destination=416+Barber+Ln%2C+Milpitas%2C+CA+95035&travelmode=walking&dir_action=navigate');
+  getLocation(from, cuisine, distance, price)
+  .then(function(result){
+    let end = chooseLocation(result);
+    startNav(res, from, end, transport);
+  })
+  .catch(error => console.log('error ' + error));
+  console.log(transport);
   console.log(from);
 });
 
@@ -72,10 +68,36 @@ function getCategories() {
     .then(result => result.json());
 }
 
-function getAutocomplete(input) {
-  return fetch("https://api.locationiq.com/v1/autocomplete.php?" + "key=" + GEOCODING_KEY + "&q=" + input)
-  .then(checkStatus)
-  .then(result => result.json());
+function getLocation(address, cuisine, radius, price) {
+  let requestOptions = {
+    method: 'GET',
+    headers:{
+      'Authorization' : 'Bearer ' + API_KEY
+    },
+    redirect: 'follow'
+  };
+
+  // add open_now=true once completed
+  let url = 'https://api.yelp.com/v3/businesses/search?&limit=50&category=restaurant';
+  return fetch(url + '&location=' + address + '&radius=' + radius + '&term=' + cuisine + '&price=' + price, requestOptions)
+    .then(checkStatus)
+    .then(result => result.json());
+}
+
+function chooseLocation(locationData) {
+  let goodPlaces = locationData.businesses.filter(function (entry) {
+    return entry.rating >= 3.5 && entry.location.address1 != null;
+  });
+  let size = Object.keys(goodPlaces).length;
+  let randIndex = Math.floor(Math.random() * size);
+  return goodPlaces[randIndex].location.display_address;
+}
+
+function startNav(res, start, end, transport) {
+  console.log(end.join(', '));
+  console.log(transport);
+  res.redirect('https://www.google.com/maps/dir/?api=1&origin=' +
+  start + '&destination=' + end.join(', ') + '&travelmode=' + transport + '&dir_action=navigate');
 }
 
 /**
--- public/index.js
+++ public/index.js
@@ -8,7 +8,6 @@
  * functions" are an exception, you may keep the function names/comments of id/qs/qsa/gen)
  */
 
-//remember to use Bearer API_KEY
 "use strict";
 
 (function () {
@@ -24,7 +23,8 @@
   function init() {
     getCategories();
     id('location').addEventListener('click', getLocation);
-    name("address")[0].addEventListener("input", getAutocomplete);
+    //uncomment for Google Autocomplete (auto charges after certain # of searches)
+    //initialize();
   }
 
   /**
@@ -105,124 +105,17 @@
     }
   }
 
-  //autocomplete address box
-  function getAutocomplete() {
-    fetch('/autocomplete', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          address: id('myInput').value
-       })
-      })
-      .then(checkStatus)
-      .then(res => res.json())
-      .catch(error => console.log('error', error));
+//Location Autocomplete
+  function initialize() {
+    var input = document.querySelector('.address input');
+    var options = {
+      types: ['(cities)'],
+      componentRestrictions: {country: 'usa'}
+    };
+    new google.maps.places.Autocomplete(input);
   }
 
-  function autocomplete(inp, arr) {
-    /*the autocomplete function takes two arguments,
-    the text field element and an array of possible autocompleted values:*/
-    var currentFocus;
-    /*execute a function when someone writes in the text field:*/
-    inp.addEventListener("input", function (e) {
-      var a, b, i, val = this.value;
-      /*close any already open lists of autocompleted values*/
-      closeAllLists();
-      if (!val) {
-        return false;
-      }
-      currentFocus = -1;
-      /*create a DIV element that will contain the items (values):*/
-      a = document.createElement("DIV");
-      a.setAttribute("id", this.id + "autocomplete-list");
-      a.setAttribute("class", "autocomplete-items");
-      /*append the DIV element as a child of the autocomplete container:*/
-      this.parentNode.appendChild(a);
-      /*for each item in the array...*/
-      for (i = 0; i < arr.length; i++) {
-        /*check if the item starts with the same letters as the text field value:*/
-        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-          /*create a DIV element for each matching element:*/
-          b = document.createElement("DIV");
-          /*make the matching letters bold:*/
-          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-          b.innerHTML += arr[i].substr(val.length);
-          /*insert a input field that will hold the current array item's value:*/
-          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-          /*execute a function when someone clicks on the item value (DIV element):*/
-          b.addEventListener("click", function (e) {
-            /*insert the value for the autocomplete text field:*/
-            inp.value = this.getElementsByTagName("input")[0].value;
-            /*close the list of autocompleted values,
-            (or any other open lists of autocompleted values:*/
-            closeAllLists();
-          });
-          a.appendChild(b);
-        }
-      }
-    });
-    /*execute a function presses a key on the keyboard:*/
-    inp.addEventListener("keydown", function (e) {
-      var x = document.getElementById(this.id + "autocomplete-list");
-      if (x) x = x.getElementsByTagName("div");
-      if (e.keyCode == 40) {
-        /*If the arrow DOWN key is pressed,
-        increase the currentFocus variable:*/
-        currentFocus++;
-        /*and and make the current item more visible:*/
-        addActive(x);
-      } else if (e.keyCode == 38) { //up
-        /*If the arrow UP key is pressed,
-        decrease the currentFocus variable:*/
-        currentFocus--;
-        /*and and make the current item more visible:*/
-        addActive(x);
-      } else if (e.keyCode == 13) {
-        /*If the ENTER key is pressed, prevent the form from being submitted,*/
-        e.preventDefault();
-        if (currentFocus > -1) {
-          /*and simulate a click on the "active" item:*/
-          if (x) x[currentFocus].click();
-        }
-      }
-    });
 
-    function addActive(x) {
-      /*a function to classify an item as "active":*/
-      if (!x) return false;
-      /*start by removing the "active" class on all items:*/
-      removeActive(x);
-      if (currentFocus >= x.length) currentFocus = 0;
-      if (currentFocus < 0) currentFocus = (x.length - 1);
-      /*add class "autocomplete-active":*/
-      x[currentFocus].classList.add("autocomplete-active");
-    }
-
-    function removeActive(x) {
-      /*a function to remove the "active" class from all autocomplete items:*/
-      for (var i = 0; i < x.length; i++) {
-        x[i].classList.remove("autocomplete-active");
-      }
-    }
-
-    function closeAllLists(elmnt) {
-      /*close all autocomplete lists in the document,
-      except the one passed as an argument:*/
-      var x = document.getElementsByClassName("autocomplete-items");
-      for (var i = 0; i < x.length; i++) {
-        if (elmnt != x[i] && elmnt != inp) {
-          x[i].parentNode.removeChild(x[i]);
-        }
-      }
-    }
-    /*execute a function when someone clicks in the document:*/
-    document.addEventListener("click", function (e) {
-      closeAllLists(e.target);
-    });
-  }
 
   /** ------------------------------ Helper Functions  ------------------------------ */
   /**
