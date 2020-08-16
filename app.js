8b24bca45eb868b8a17fada9c0dc28dbfb603612
kchou97 <kchou97@uw.edu>
2020-08-16 01:04:53 -0700

    added console.log

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(bodyparser.urlencoded({ extended: false }))
 app.listen(3000, function(req, res){
   console.log("Server is running on port 3000");
   console.log(API_KEY);
+  console.log(__dirname);
 });
 
 /**
